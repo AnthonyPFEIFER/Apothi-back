dd9f46ab642c80ff8dde6e8e3295692d81b0ed08
AnthonyPFEIFER <anthhony.pfeifer@gmail.com>
2021-11-16 13:51:20 +0100

    Corrections

--- api/src/main/java/com/apothicaires/api/ApothicairesApplication.java
+++ api/src/main/java/com/apothicaires/api/ApothicairesApplication.java
@@ -2,12 +2,26 @@ package com.apothicaires.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 @SpringBootApplication
-public class ApothicairesApplication {
+public class ApothicairesApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApothicairesApplication.class, args);
 	}
+	@Override
+	public void run(String... args) throws Exception {
 
+		try {
+			System.out.println("Hello World");
+		} catch (Exception e) {
+			System.out.println("Ca fonctionne pas");
+			throw new Exception("I am Exception Alpha!");
+		}
+	}
 }
---
+++ api/src/main/resources/data.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS billionaires;
+
+CREATE TABLE billionaires (
+                              id INT AUTO_INCREMENT  PRIMARY KEY,
+                              first_name VARCHAR(250) NOT NULL,
+                              last_name VARCHAR(250) NOT NULL,
+                              career VARCHAR(250) NOT NULL,
+                              mail VARCHAR(250) NOT NULL,
+                              password VARCHAR(250) NOT NULL
+);
+
+INSERT INTO billionaires (first_name, last_name, career, mail, password) VALUES
+                                                                             ('Anthony', 'Pfeifer', 'Apprenti développeur', 'anthony@gmail.com', 'anthony'),
+                                                                             ('Julien', 'Bergès', 'Apprenti développeur ascendant security guy', 'julien@gmail.com', 'julien'),
+                                                                             ('Elliot', 'Bobillet', 'Pâtes et riz industries', 'elliot@gmail.com', 'elliot'),
+                                                                             ('David', 'Goodenough', 'Spassimal Company', 'david@gmail.com', 'david');
