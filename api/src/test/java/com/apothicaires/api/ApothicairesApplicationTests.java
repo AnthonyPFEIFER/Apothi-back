40a8cb4d9b3d50886b16b1c07beb478a1af64a1e
AnthonyPFEIFER <anthhony.pfeifer@gmail.com>
2021-11-16 11:35:35 +0100

    Second commit

---
+++ api/src/test/java/com/apothicaires/api/ApothicairesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apothicaires.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApothicairesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
