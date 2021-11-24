583366f6f6e9422eab3de1c383967e4d47440140
Anthony PFEIFER <anthhony.pfeifer@gmail.com>
2021-11-24 10:50:34 +0100

    refactor + clean project

--- api/src/main/java/com/apothicaires/api/repository/PhotoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.apothicaires.api.repository;
-
-import com.apothicaires.api.model.Photo;
-import com.apothicaires.api.model.Tatouage;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-public interface PhotoRepository extends CrudRepository<Photo, Integer> {
-}
