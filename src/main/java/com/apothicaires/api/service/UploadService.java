2ec93fa4aa599ff0ed26797f934e5e0a5a5e7cf5
Anthony PFEIFER <anthhony.pfeifer@gmail.com>
2021-11-25 14:45:45 +0100

    work in progress create tatouage with photo

---
+++ src/main/java/com/apothicaires/api/service/UploadService.java
@@ -0,0 +1,23 @@
+package com.apothicaires.api.service;
+
+import com.apothicaires.api.model.Photo;
+import com.apothicaires.api.model.Tatouage;
+import lombok.Data;
+import org.springframework.stereotype.Service;
+
+@Data
+@Service
+public class UploadService {
+
+    private TatouageService tatouageService;
+    private PhotoService photoService;
+
+    public Tatouage multipleCreate(Photo photo, Tatouage tatouage){
+        Tatouage generatedTatouage = this.tatouageService.createNewTatouage(tatouage);
+        Integer idGenerated = generatedTatouage.getTatouage_id();
+        Photo photoWithId = photo;
+        photo.setTatouage_id(idGenerated);
+        this.photoService.createNewPhoto(photoWithId);
+        return generatedTatouage;
+    }
+}
